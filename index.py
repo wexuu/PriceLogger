84c3cea4eb821ef0cabbdca17335000e72b9d85a
wex <kubawxl@gmail.com>
2024-11-26 21:16:32 +0100

    f

---
+++ index.py
@@ -0,0 +1,54 @@
+import tkinter
+from tkinter import ttk
+import sv_ttk
+from var import project_path
+from func.createdb import createdb
+#Everything main window related. Mostly misc
+root = tkinter.Tk()
+root.geometry("800x500")
+root.maxsize(900, 800)
+
+#Checking whether database exists />/ creating it 
+print("hhhhhhhh")
+print(project_path)
+createdb()
+
+tabControl = ttk.Notebook(root) 
+tabAdd = ttk.Frame(tabControl) 
+tabDel = ttk.Frame(tabControl) 
+tabView = ttk.Frame(tabControl) 
+
+tabControl.add(tabView, text ='Przeglądaj') 
+tabControl.add(tabAdd, text ='Dodaj') 
+tabControl.add(tabDel, text ='Usuń') 
+tabControl.pack(expand = 1, fill ="both") 
+  
+ttk.Label(tabView,  
+          text ="przegladanie itemow").grid(column = 0,  
+                               row = 0, 
+                               padx = 30, 
+                               pady = 30)   
+ttk.Label(tabAdd,  
+          text ="dodawanie itemow").grid(column = 0,  
+                               row = 0, 
+                               padx = 30, 
+                               pady = 30)   
+ttk.Label(tabDel, 
+          text ="usuwanie itemow").grid(column = 0, 
+                                    row = 0,  
+                                    padx = 30, 
+                                    pady = 30) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
---
+++ var.py
@@ -0,0 +1,3 @@
+import os
+
+project_path: str = os.path.abspath(os.path.dirname(__file__))
