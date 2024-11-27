dce47bd34a86f6eab0b87f2636bc060d8a3c71da
wexuuu <47903842+wexuu@users.noreply.github.com>
2024-11-27 23:33:12 +0100

    yay

--- index.py
+++ index.py
@@ -1,43 +1,63 @@
-import tkinter
+import tkinter as tk
 from tkinter import ttk
-import sv_ttk, os
+import os
 from var import project_path
-import func
 #Everything main window related. Mostly misc
-root = tkinter.Tk()
+root = tk.Tk()
 root.geometry("800x500")
 root.maxsize(1000, 900)
 
 #Checking whether database exists />/ creating it 
 if(os.path.exists(project_path+"\\db.db")==False):
-    func.createdb()
-#hihi
+    import func.createdb
+    print(func.createdb.createdb())
+
 
 tabControl = ttk.Notebook(root) 
-tabAdd = ttk.Frame(tabControl) 
-tabDel = ttk.Frame(tabControl) 
-tabView = ttk.Frame(tabControl) 
+tabAdd = tk.Frame(tabControl) 
+tabDel = tk.Frame(tabControl) 
+tabView = tk.Frame(tabControl) 
 
 tabControl.add(tabView, text ='Przeglądaj') 
 tabControl.add(tabAdd, text ='Dodaj') 
 tabControl.add(tabDel, text ='Usuń') 
 tabControl.pack(expand = 1, fill ="both") 
-  
-ttk.Label(tabView,  
-          text ="przegladanie itemow").grid(column = 0,  
-                               row = 0, 
-                               padx = 30, 
-                               pady = 30)   
-ttk.Label(tabAdd,  
-          text ="dodawanie itemow").grid(column = 0,  
-                               row = 0, 
-                               padx = 30, 
-                               pady = 30)   
-ttk.Label(tabDel, 
-          text ="usuwanie itemow").grid(column = 0, 
-                                    row = 0,  
-                                    padx = 30, 
-                                    pady = 30) 
+
+tk.Label(tabView,  text ="przegladanie itemow", width=40).grid(padx = 30, pady = 30,)   
+tk.Label(tabAdd,  
+    text ="dodawanie itemow").grid(column = 0,  row = 0,pady = 30)   
+tk.Label(tabDel, 
+    text ="usuwanie itemow").grid(column = 0, row = 0)
+
+name_var=tk.StringVar()
+passw_var=tk.StringVar()
+
+
+def submit():
+
+    name=name_var.get()
+    
+    print("The name is : " + name)
+    
+    name_var.set("")
+
+
+name_label = tk.Label(tabView, text = 'Username', font=('calibre',10, 'bold'), width=30).grid(padx=20,pady=20)
+
+# creating a entry for input
+# name using widget Entry
+name_entry = tk.Entry(tabView,textvariable = name_var, font=('calibre',10,'normal'), width=30).grid(padx=20,pady=20)
+
+sub_btn=tk.Button(tabView,text = 'Submit', command = submit).grid(padx=20,pady=20)
+
+
+
+
+
+
+
+
+
 
 
 
